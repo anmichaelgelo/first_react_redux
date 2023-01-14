517bbba9c6e25ebbe635df57408cb13897e62c00
Michael Agoncillo <108847743+anmichaelgelo@users.noreply.github.com>
2023-01-14 15:24:08 +0000

    Initial commit

---
+++ src/Store.js
@@ -0,0 +1,6 @@
+import { configureStore } from '@reduxjs/toolkit'
+import counterReducer from './features/counterSlice'
+
+export const Store = configureStore({
+    reducer: counterReducer
+})
---
+++ src/components/Counter.jsx
@@ -0,0 +1,29 @@
+import React, { useState } from 'react'
+import { useSelector, useDispatch } from "react-redux";
+import { increment, decrement, incrementByAmount } from '../features/counterSlice'
+
+export default function Counter(){
+    const count = useSelector((state) => state.value)
+    const dispatch = useDispatch()
+    const [ input, setInput ] = useState(0)
+
+    const byAmountSubmit = (e) => {
+        e.preventDefault()
+        dispatch(incrementByAmount(Number(input)))
+        setInput(0)
+    }
+    
+    return (
+        <>
+            <h1>{count}</h1>
+            <button type='button' onClick={() => dispatch(increment())}>Increment</button>
+            <button type='button' onClick={() => dispatch(decrement())}>Decrement</button>
+
+            <form onSubmit={(e) => byAmountSubmit(e)}>
+                <input type="number" onChange={(e) => setInput(e.target.value)} />
+                <button type='submit'>Submit</button>
+            </form>
+
+        </>
+    )
+}
